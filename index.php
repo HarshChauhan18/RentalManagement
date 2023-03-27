8924bc19a550b12e0200835a629583bdcd680fb1
HarshChauhan18 <harshchauhan3974@gmail.com>
2023-03-27 08:19:42 +0530

    Added 404 Page Not Found

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
                   <i class="fa fa-search" aria-hidden="true"></i>
                 </button>
               </form>
-              <a href="" class="order_online">
+              <a href="order.php" class="order_online">
                 Order Online
               </a>
             </div>
--- vehicles.php
+++ vehicles.php
@@ -62,7 +62,7 @@
                 <a class="nav-link" href="index.php">Home </a>
               </li>
               <li class="nav-item active">
-                <a class="nav-link" href="vechicles.php">VEHICLES <span class="sr-only">(current)</span> </a>
+                <a class="nav-link" href="vehicles.php">VEHICLES <span class="sr-only">(current)</span> </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="about.php">About</a>
