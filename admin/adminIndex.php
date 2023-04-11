9227f69252f9876171eb4476430aedba01b35114
HarshChauhan18 <harshchauhan3974@gmail.com>
2023-04-11 08:25:48 +0530

    Added Admin Dashboard

---
+++ admin/adminIndex.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/adminStyle.css">
+</head>
+<body>
+	<div class="header">
+		<h1>Admin Dashboard</h1>
+		<ul>
+			<li><a href="#">Add Vehicle</a></li>
+			<li><a href="#">Verify Booking Requests</a></li>
+			<li><a href="#">Delete Vehicle</a></li>
+		</ul>
+	</div>
+	<div class="content">
+		<!-- Content goes here -->
+	</div>
+	<div class="footer">
+		<p>&copy; 2023 Admin Dashboard</p>
+	</div>
+</body>
+</html>
